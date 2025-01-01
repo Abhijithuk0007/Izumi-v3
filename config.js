a919f853b36f151cbdc16d701a7b1138c6224284
Abhijithuk0007 <107612856+Abhijithuk0007@users.noreply.github.com>
2025-01-01 22:42:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
   BRANCH: "main",
-  MODE: (process.env.MODE || 'private').toLowerCase(),
+  MODE: (process.env.MODE || 'public').toLowerCase(),
   ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
   LOG_MSG: toBool(process.env.LOG_MSG) || true,
   READ_CMD: toBool(process.env.READ_CMD),
-  SESSION_ID: process.env.SESSION_ID || "Session id",
+  SESSION_ID: process.env.SESSION_ID || "izumi~wc537BQY",
   MENU_URL: process.env.MENU_URL || "https://i.imgur.com/cO0TZJv.jpeg",
   CAPTION: process.env.CAPTION || "Iᴢᴜᴍɪ",
   READ_MSG: toBool(process.env.READ_MSG),
